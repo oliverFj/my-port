b492157d617d5e76a8e82508c6494e68c0a045e8
OliverF <oliver@fjordside.dk>
2023-06-19 12:57:24 +0200

    Added a components folder and a Layout.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import { useReducer, useEffect, createContext } from 'react';
+import { useReducer, createContext } from 'react';
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import BlogAndWritingsPage from "./pages/BlogAndWritingsPage";
 import DesignAndArtPage from "./pages/DesignAndArtPage";
 import FirebaseInput from "./pages/FirebaseInput";
 import BlogDetailPage from "./pages/BlogDetailPage";
+import Layout from "./Layout";
 
-
-// Define your context and provide a default value for your reducer
 export const AppContext = createContext();
 
-// Define your reducer function
 function appReducer(state, action) {
   switch (action.type) {
     case 'SET_PAGE':
@@ -27,9 +24,7 @@ function appReducer(state, action) {
   }
 }
 
-
 function App() {
-  // Initialize your reducer and state with the default value and function returned from useReducer
   const [state, dispatch] = useReducer(appReducer, {
     headline: '',
     sidebar: '',
@@ -37,52 +32,40 @@ function App() {
   });
 
   return (
-    // Wrap your app in the provider and pass the current state and dispatch function to the value prop of the provider object
     <AppContext.Provider value={{ state, dispatch }}>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage dispatch={dispatch} />} />
-          <Route path="/about" element={<AboutPage dispatch={dispatch} />} />
-          <Route path="/blog" element={<BlogAndWritingsPage dispatch={dispatch} />} />
-          <Route path="/design" element={<DesignAndArtPage dispatch={dispatch} />} />
-          <Route path="/input" element={<FirebaseInput dispatch={dispatch} />} />
-          <Route path="/blog/:id" element={<BlogDetailPage />} />
+          <Route path="/" element={
+            <Layout headline={state.headline} sidebar={state.sidebar} main={state.main}>
+              <HomePage dispatch={dispatch} />
+            </Layout>
+          } />
+          <Route path="/about" element={
+            <Layout headline={state.headline} sidebar={state.sidebar} main={state.main}>
+              <AboutPage dispatch={dispatch} />
+            </Layout>
+          } />
+          <Route path="/blog" element={
+            <Layout headline={state.headline} sidebar={state.sidebar} main={state.main}>
+              <BlogAndWritingsPage dispatch={dispatch} />
+            </Layout>
+          } />
+          <Route path="/design" element={<DesignAndArtPage />} />
+          <Route path="/input" element={
+            <Layout headline={state.headline} sidebar={state.sidebar} main={state.main}>
+              <FirebaseInput dispatch={dispatch} />
+            </Layout>
+          } />
+          <Route path="/blog/:id" element={
+            <Layout headline={state.headline} sidebar={state.sidebar} main={state.main}>
+              <BlogDetailPage />
+            </Layout>
+          } />
           {/* // Other routes... */}
         </Routes>
-
-        <div className="flex items-center min-h-screen justify-center">
-          <div className="w-[1000px] h-[750px] flex flex-col border-black">
-            <header className="flex items-center w-full h-12 bg-orange-200 border-2 border-black">
-              <h1 className="text-black text-xl text-left font-bold px-2">{state.headline}</h1>
-            </header>
-
-            <div className="flex flex-row flex-grow border-l-2 border-r-2 border-black">
-              <aside className="w-1/3 border-r-2 border-black flex flex-col">
-
-                {state.sidebar}
-
-                {/*  Other menu items... */}
-              </aside>
-
-              <main className="w-2/3">
-                {state.main}
-              </main>
-            </div>
-
-              <footer className="w-full h-24 border-t-2 border-black">
-                <Link to="/">
-                  <img src="/FjordsideLogo.png" alt="Descriptive text" className="w-52 mx-auto float-right mt-1" />
-                </Link>
-              </footer>
-
-          </div>
-        </div>
       </Router>
     </AppContext.Provider>
   );
 }
-
-// Export your App component
-// This is what will be imported into index.js
 
 export default App;
--- src/pages/DesignAndArtPage.js
+++ src/pages/DesignAndArtPage.js
@@ -1,73 +1,53 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ref, onValue } from 'firebase/database';
+import db from '../firebase'; // adjust the path to your firebase file
+import Menu from '../components/menu';
+import Gallery from '../components/gallery';
+
+import { useContext } from 'react';
+import Layout from '../Layout'; // adjust the path to your Layout file
+import { AppContext } from '../App'; // adjust the path to your App file
 
 function DesignAndArtPage(props) {
+  const { state, dispatch } = useContext(AppContext);
+
+  const menuItems = [
+    { link: '/design/3d-characters', label: '3D Characters' },
+    { link: '/design/art', label: 'Art' },
+    { link: '/design/product-design', label: 'Product Design' },
+    { link: '/', label: 'Back' },
+  ];
+
+  const [galleryItems, setGalleryItems] = useState([]);
+
   useEffect(() => {
-    props.dispatch({ 
-      type: 'SET_PAGE', 
-      payload: {
-        headline: "Design & Art",
-        sidebar: (
-          <div className="flex flex-col space-y-0">
-            {/* These links can be used to filter the gallery based on category */}
-            <Link to="/design-art/3d-characters" className="block hover:bg-gray-200">
-              <div className="flex items-center h-12 text-center border-b-2 border-black text-black font-bold px-2 cursor-pointer">
-                3D Characters
-              </div>
-            </Link>
-
-            <Link to="/design-art/art" className="block hover:bg-gray-200">
-              <div className="flex items-center h-12 text-center border-b-2 border-black text-black font-bold px-2 cursor-pointer">
-                Art
-              </div>
-            </Link>
-
-            <Link to="/design-art/product-design" className="block hover:bg-gray-200">
-              <div className="flex items-center h-12 text-center border-b-2 border-black text-black font-bold px-2 cursor-pointer">
-                Product Design
-              </div>
-            </Link>
-
-            <Link to="/" className="block hover:bg-gray-200">
-              <div className="flex items-center h-12 text-center border-b-2 border-black text-black font-bold px-2 cursor-pointer">
-                Back
-              </div>
-            </Link>
-
-
-          </div>
-        ),
-        main: (
-          <div className="space-y-4">
-
-            
-            {/* Gallery */}
-            <div className="grid grid-cols-3 gap-4 p-4">
-              {/* Replace these placeholders with actual data from your database */}
-              {Array.from({ length: 6 }, (_, i) => (
-                <div key={i} className="border-2 p-2 border-black">
-                  <img src={`/thumbnails/${i}.jpg`} alt={`Artwork ${i}`} className="w-full h-48 object-cover" />
-                  <h3 className="mt-2 text-center font-bold">Artwork {i}</h3>
-                </div>
-              ))}
-            </div>
-
-            {/* Pagination */}
-            <div className="flex justify-center space-x-2 mt-4">
-              {/* Replace these placeholders with actual page numbers */}
-              {Array.from({ length: 5 }, (_, i) => (
-                <Link key={i} to={`/design-art?page=${i+1}`} className="block w-8 h-8 border text-center line-height[32px] hover:bg-gray-200">
-                  {i + 1}
-                </Link>
-              ))}
-            </div>
-          </div>
-        ),
-      } 
+    // Fetch data from Realtime Database
+    const dbRef = ref(db, 'art');
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      const items = [];
+      for (let id in data) {
+        items.push({ image: data[id].imageThumbnail, alt: data[id].name, title: data[id].name });
+      }
+      console.log("Mapped items:", items);
+      setGalleryItems(items);
     });
   }, []);
 
-  return null; // The actual rendering is handled by the App component
+  useEffect(() => {
+    dispatch({ 
+      type: 'SET_PAGE', 
+      payload: {
+        headline: "Design & Art",
+        sidebar: <Menu items={menuItems} />,
+        main: <Gallery items={galleryItems} page={1} totalPages={5} />,
+      } 
+    });
+  }, [galleryItems]);
+
+  return (
+    <Layout headline={state.headline} sidebar={state.sidebar} main={state.main} />
+  );
 }
 
 export default DesignAndArtPage;
